f523f673f08b68a420c0e5df880dd70684880c1e
Simon Richter <Simon.Richter@hogyros.de>
2022-04-24 17:54:40 +0200

    Create and map window

--- display/src/x11_setup.c
+++ display/src/x11_setup.c
@@ -7,9 +7,14 @@
 #include "bss2kdpy.h"
 
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
 bool x11_setup(struct global *g)
 {
+	/** @todo constants? */
+	int const tex_width = 480;
+	int const tex_height = 360;
+
 	g->x11.display = XOpenDisplay(NULL);
 
 	if(g->x11.display == NULL)
@@ -27,8 +32,8 @@ bool x11_setup(struct global *g)
 		g->x11.window = XCreateWindow(
 				/* display */	g->x11.display,
 				/* parent */	root,
-				/* x, y */	100, 100,
-				/* w, h */	200, 200,
+				/* x, y */	0, 0,
+				/* w, h */	tex_width, tex_height,
 				/* border w */	0,
 				/* depth */	CopyFromParent,
 				/* class */	InputOutput,
@@ -38,6 +43,37 @@ bool x11_setup(struct global *g)
 
 		if(g->x11.window == None)
 			goto fail;
+	}
+
+	{
+		XSizeHints *size_hints = XAllocSizeHints();
+		if(!size_hints)
+			goto fail;
+
+		size_hints->min_width = tex_width;
+		size_hints->min_height = tex_height;
+		size_hints->width_inc = tex_width;
+		size_hints->height_inc = tex_height;
+		size_hints->base_width = tex_width;
+		size_hints->base_height = tex_height;
+		size_hints->flags = PMinSize|PResizeInc|PBaseSize;
+
+		XSetStandardProperties(
+				g->x11.display,
+				g->x11.window,
+				"BSS2k Display",
+				"BSS2k Display",
+				None,
+				g->argv,
+				g->argc,
+				size_hints);
+
+		XFree(size_hints);
+
+	}
+
+	{
+		XMapWindow(g->x11.display, g->x11.window);
 	}
 
 	return true;
