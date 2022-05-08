3a7560cca9b4d9cf942e3800b5218230ccdbab44
Simon Richter <Simon.Richter@hogyros.de>
2022-05-08 14:26:49 +0200

    Add PCIe block and support components

---
+++ logic/board_phi/top.sdc
@@ -0,0 +1,5 @@
+# input clocks are already defined by pcie.sdc
+
+derive_pll_clocks
+
+derive_clock_uncertainty
--- logic/gen.sh
+++ logic/gen.sh
@@ -4,3 +4,14 @@ if ! which quartus_sh 2>/dev/null; then
 	PATH=/opt/altera/16.1/quartus/bin:$PATH
 fi
 qmegawiz -silent cpu/altera_registers.vhd
+
+qmegawiz -silent board_phi/pll.vhd
+qmegawiz -silent board_phi/pcie.vhd
+qmegawiz -silent board_phi/pcie_serdes.vhd
+qmegawiz -silent board_phi/pcie_reconfig.vhd
+
+# patch frequency of fixedclk_serdes
+sed -i -e '/fixedclk_serdes/s/100/125/' board_phi/pcie.sdc
+
+# copy reset controller example
+cp board_phi/pcie_examples/chaining_dma/pcie_rs_hip.vhd board_phi/
