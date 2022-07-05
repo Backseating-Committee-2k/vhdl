80e4d28b956d45261f718a36babedc9a9450a00f
Simon Richter <Simon.Richter@hogyros.de>
2022-07-05 18:36:35 +0200

    Reflow list of input files for sim

--- logic/sim.sh
+++ logic/sim.sh
@@ -4,7 +4,14 @@ ghdl -a --std=08 cpu/bss2k.vhdl cpu/mem_arbiter.vhdl cpu/tb_mem_arbiter.vhdl
 ghdl -e --std=08 tb_mem_arbiter
 ghdl -r --std=08 tb_mem_arbiter --wave=tb_mem_arbiter.ghw
 
-ghdl -a --std=08 cpu/bss2k.vhdl cpu/cpu_pipelined.vhdl cpu/cpu_sequential.vhdl cpu/registers.vhdl cpu/tb_cpu.vhdl cpu/tb_cpu_sim_pipe.vhdl cpu/tb_cpu_sim_seq.vhdl
+ghdl -a --std=08 \
+	cpu/bss2k.vhdl \
+	cpu/cpu_pipelined.vhdl \
+	cpu/cpu_sequential.vhdl \
+	cpu/registers.vhdl \
+	cpu/tb_cpu.vhdl \
+	cpu/tb_cpu_sim_pipe.vhdl \
+	cpu/tb_cpu_sim_seq.vhdl
 ghdl -e --std=08 tb_cpu_sim_seq
 ghdl -r --std=08 tb_cpu_sim_seq --wave=tb_cpu_seq.ghw
 ghdl -e --std=08 tb_cpu_sim_pipe
